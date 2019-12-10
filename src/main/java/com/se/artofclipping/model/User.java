75decd2f943b0ca7cfbac41e54cfbc5128ff57b1
Mateusz Klimas <matiklim.98@gmail.com>
2019-12-10 16:50:37 +0100

    added user class

--- src/main/java/com/se/artofclipping/model/User.java
+++ src/main/java/com/se/artofclipping/model/User.java
@@ -2,10 +2,8 @@ package com.se.artofclipping.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Set;
 
 @Data
 @Entity
@@ -14,8 +12,14 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String name;
     private String secondName;
     private String email;
     private String password;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
 }
