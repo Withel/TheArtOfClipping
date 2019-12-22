1c4c328264af3f422b720c051e55cb36b6987310
Mateusz Klimas <matiklim.98@gmail.com>
2019-12-22 14:48:24 +0100

    Changes for services, also very basic displaying on index page.

--- src/main/java/com/se/artofclipping/repositories/ServiceRepository.java
+++ src/main/java/com/se/artofclipping/repositories/ServiceRepository.java
@@ -4,6 +4,10 @@ import com.se.artofclipping.model.Service;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ServiceRepository extends CrudRepository<Service, Long> {
+
+    List<Service> findAllByType(Character type);
 }
