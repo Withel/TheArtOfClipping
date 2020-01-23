7a1d902a98ba0940b5b4f475dc1900ce27f3fff7
tomaszsojka <tomek.s@windowslive.com>
2020-01-23 02:09:33 +0100

    Admin Hds Email change and Hds Password change forms added. Forms do nothing yet.

--- src/main/java/com/se/artofclipping/controllers/AdminController.java
+++ src/main/java/com/se/artofclipping/controllers/AdminController.java
@@ -220,5 +220,61 @@ public class AdminController {
         return "user/admin/adminpage";
     }
 
+    @GetMapping("user/admin/changeHdsEmailView")
+    public String adminChangeHdsEmailView(Model model){
+        model.addAttribute("newUser",new User());
+        model.addAttribute("listHds",adminService.listHairdressers());
+        return "user/admin/adminChangeHdsEmail";
+    }
+
+    @PostMapping("user/admin/changeHdsEmail")
+    public String adminChangeHdsEmail(@ModelAttribute User user,Model model) {
+
+        //TODO
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        User userExists = adminService.findUserByEmail(user.getEmail());
+//        if (userExists == null) {
+//            model.addAttribute("newUser",new User());
+//            model.addAttribute("listHds",adminService.listHairdressers());
+//            return "user/admin/adminChangeHdsEmail";
+//        }
+//
+//        else if(!adminService.changeHdsSurname(userExists,user.getPassword(),auth.getName(),user.getSurname()))
+//        {
+//            model.addAttribute("newUser",new User());
+//            model.addAttribute("listHds",adminService.listHairdressers());
+//            return "user/admin/adminChangeHdsEmail";
+//        }
+        return "user/admin/adminpage";
+    }
+
+    @GetMapping("user/admin/changeHdsPasswordView")
+    public String adminChangeHdsPasswordView(Model model){
+        model.addAttribute("newUser",new User());
+        model.addAttribute("listHds",adminService.listHairdressers());
+        return "user/admin/adminChangeHdsPassword";
+    }
+
+    @PostMapping("user/admin/changeHdsPassword")
+    public String adminChangeHdsPassword(@ModelAttribute User user,Model model) {
+
+        //TODO
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        User userExists = adminService.findUserByEmail(user.getEmail());
+//        if (userExists == null) {
+//            model.addAttribute("newUser",new User());
+//            model.addAttribute("listHds",adminService.listHairdressers());
+//            return "user/admin/adminChangeHdsEmail";
+//        }
+//
+//        else if(!adminService.changeHdsSurname(userExists,user.getPassword(),auth.getName(),user.getSurname()))
+//        {
+//            model.addAttribute("newUser",new User());
+//            model.addAttribute("listHds",adminService.listHairdressers());
+//            return "user/admin/adminChangeHdsEmail";
+//        }
+        return "user/admin/adminpage";
+    }
+
 }
 
